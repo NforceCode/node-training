7fe8a65ac358b7de83bcd865f317638d2e0ede77
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-02-05 20:07:37 +0200

    Clear project

--- Hackermans.js
+++
@@ -1,5 +0,0 @@
-class Hackermans {
-  static hack() { console.log('HACKERMANS I`m in')};
-}
-
-exports.Hackermans = Hackermans;
--- MyMath.js
+++
@@ -1,5 +0,0 @@
-class MyMath{
-  static sum = (...args)=> args.reduce((a,i)=>a+i,0);
-}
-
-exports.MyMath = MyMath;
